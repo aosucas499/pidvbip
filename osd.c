e9eb0b3a50be73166cc31007f2790714d6e25e2f
dni1337 <15782082+dni1337@users.noreply.github.com>
2020-03-27 15:11:14 +0100

    Update osd.c

--- osd.c
+++ osd.c
@@ -158,18 +158,18 @@ int32_t render_paragraph(GRAPHICS_RESOURCE_HANDLE img, const char *text, const u
        /* No spaces, within img_w. Just go through character by character */
        line_length = 0;
        do {
-         line_length = line_length+10;
+         line_length = line_length+1;
          s = graphics_resource_text_dimensions_ext(img, text, line_length, &width, &height, text_size);
          if (s != 0) return s;
        } while (width < img_w);
 
-       do {
+       /*do {
          line_length--;
          s = graphics_resource_text_dimensions_ext(img, text, line_length, &width, &height, text_size);
          if (s != 0) return s;
        } while (width > img_w);
-	     
-       //line_length--;
+	 */    
+       line_length--;
      } else {
        /* We have at least one space, so can split line on a space */
        width = 0;
