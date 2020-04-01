7ba36d381c322b9c1820f6b0b6228cfaf480db46
dni1337 <15782082+dni1337@users.noreply.github.com>
2020-04-01 12:27:21 +0200

    Update osd.c

--- osd.c
+++ osd.c
@@ -175,7 +175,7 @@ int32_t render_paragraph(GRAPHICS_RESOURCE_HANDLE img, const char *text, const u
        width = 0;
        line_length = space - text;
 
-       while (width < img_w) {
+       while (width < img_w && height < 50) {
          space = index(space+1,' ');
          s = graphics_resource_text_dimensions_ext(img, text, space - text, &width, &height, text_size);
          if (s != 0) return s;
