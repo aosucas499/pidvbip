1cfc475d3b22ad7f8c1f60df213dbf2906182666
dni1337 <15782082+dni1337@users.noreply.github.com>
2020-04-02 12:46:54 +0200

    Update osd.c

--- osd.c
+++ osd.c
@@ -138,13 +138,13 @@ int32_t render_paragraph(GRAPHICS_RESOURCE_HANDLE img, const char *text, const u
      width = 9999;
    } else {
      s = graphics_resource_text_dimensions_ext(img, text, text_length, &width, &height, text_size);
-     fprintf(stderr,"height1: %u\n",height)
+     fprintf(stderr,"height1: %u\n",height);
      if (s != 0) return s;
    }
 	   
    if (width <= img_w && height < 50) {
      /* We can display the whole line */
-     fprintf(stderr,"height2: %u\n",height)
+     fprintf(stderr,"height2: %u\n",height);
      line_length = text_length;
    } else {
      //fprintf(stderr,"width=%d, img_w=%d, looking for next space\n",width,img_w);
@@ -153,7 +153,7 @@ int32_t render_paragraph(GRAPHICS_RESOURCE_HANDLE img, const char *text, const u
 
      if (space) {
        s = graphics_resource_text_dimensions_ext(img, text, space-text, &width, &height, text_size);
-       fprintf(stderr,"height3: %u\n",height)
+       fprintf(stderr,"height3: %u\n",height);
        if (s != 0) return s;
      }
 
@@ -175,7 +175,7 @@ int32_t render_paragraph(GRAPHICS_RESOURCE_HANDLE img, const char *text, const u
        //line_length--;
      } else {
        /* We have at least one space, so can split line on a space */
-       fprintf(stderr,"height4: %u\n",height)
+       fprintf(stderr,"height4: %u\n",height);
        width = 0;
        line_length = space - text;
 
