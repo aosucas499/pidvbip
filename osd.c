1640c0e54768078bbdfe7b87730ee96b9a53902a
dni1337 <15782082+dni1337@users.noreply.github.com>
2020-04-02 15:41:13 +0200

    Update osd.c

--- osd.c
+++ osd.c
@@ -200,16 +200,18 @@ int32_t render_paragraph(GRAPHICS_RESOURCE_HANDLE img, const char *text, const u
          if ((width < img_w) && (height <= 30)) { line_length = space - text; }
        }
        
-       space = index(text,'\n');
+       if (height > 30) {
+         space = index(text,'\n');
 
-       if (space) {
-	 fprintf(stderr,"line_length1: %u\n",line_length);
-	 fprintf(stderr,"line_length2: %u\n",space-text);
-	 if (line_length > (space-text)) {
-           line_length = space-text;
-	   s = graphics_resource_text_dimensions_ext(img, text, line_length, &width, &height, text_size);
-           if (s != 0) return s;
-	 }
+         if (space) {
+  	   fprintf(stderr,"line_length1: %u\n",line_length);
+	   fprintf(stderr,"line_length2: %u\n",space-text);
+	   if (line_length > (space-text)) {
+             line_length = space-text;
+	     s = graphics_resource_text_dimensions_ext(img, text, line_length, &width, &height, text_size);
+             if (s != 0) return s;
+	   }
+         }
        }
 	  
      }
