b249b2c961a7769fed036f8fe962f4b0492bb8b1
dni1337 <15782082+dni1337@users.noreply.github.com>
2020-04-02 14:59:28 +0200

    Update osd.c

--- osd.c
+++ osd.c
@@ -179,14 +179,14 @@ int32_t render_paragraph(GRAPHICS_RESOURCE_HANDLE img, const char *text, const u
        width = 0;
        line_length = space - text;
 
-       while (width < img_w) {
+       while ((width < img_w) && (height <= 30)) {
          space = index(space+1,' ');
          s = graphics_resource_text_dimensions_ext(img, text, space - text, &width, &height, text_size);
          if (s != 0) return s;
 	       
          if ((width < img_w) && (height <= 30)) { line_length = space - text; }
        }
-       /*
+       
 	fprintf(stderr,"blablabla\n");
 	space = index(space+1,'\n');
 
@@ -196,7 +196,7 @@ int32_t render_paragraph(GRAPHICS_RESOURCE_HANDLE img, const char *text, const u
 	 if (line_length > (space-text)) {
            line_length = space-text;
 	 }
-       }*/
+       }
 	  
      }
    }
